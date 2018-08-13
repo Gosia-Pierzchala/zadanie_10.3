a21a67666471cd15e4ea232edaa3c599e5df90e2
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-08-13 11:25:32 +0200

    poprawki

--- src/CreateFile.java
+++ src/CreateFile.java
@@ -1,39 +1,42 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CreateFile {
     public static void main(String[] args) throws IOException {
 
-        FileWriter fw = new FileWriter("produkty.csv");
-        BufferedWriter bfw = new BufferedWriter(fw);
+        File file = new File("produkty.csv");
+        if(!file.exists()){
+        file.createNewFile();}
 
-        String line1 = "Mleko;mlekovita;2.5";
-        String line2 = "Czekolada;wedel;3.5";
-        String line3 = "Sok;hortex;5";
+        int i = 0;
+        try{
+            Scanner scan = new Scanner(file);
+            while(scan.hasNextLine()){
+                String nextLine = scan.nextLine();
+                i++;
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
 
-        bfw.write(line1);
-        bfw.newLine();
-        bfw.write(line2);
-        bfw.newLine();
-        bfw.write(line3);
-        bfw.newLine();
-        bfw.close();
+        Product[] products = new Product[i];
 
-        String[] split1 = line1.split(";");
-        double price1 = Double.valueOf(split1[2]);
-
-        String[] split2 = line2.split(";");
-        double price2 = Double.valueOf(split2[2]);
-
-        String[] split3 = line3.split(";");
-        double price3 = Double.valueOf(split3[2]);
-
-        Product[] products = new Product[3];
-        products[0] = new Product(split1[0], split1[1], price1);
-        products[1] = new Product(split2[0], split2[1], price2);
-        products[2] = new Product(split3[0], split3[1], price3);
+        try{
+            int j = 0;
+            Scanner scan = new Scanner(file);
+            while(scan.hasNextLine()){
+                String nextLine = scan.nextLine();
+                String[] split = nextLine.split(";");
+                double price = Double.valueOf(split[2]);
+                products[j] = new Product(split[0], split[1], price);
+                j++;
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
 
         String array = Arrays.toString(products);
         System.out.println(array);
